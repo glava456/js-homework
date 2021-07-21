789ba8e957b3dba5c91cae4730b2851f836e66a2
glava456 <glava456@gmail.com>
2021-07-21 15:22:19 +0300

    add metodSelect, edit 18_reversPartArray

--- 18_reversPartArray.js
+++ 18_reversPartArray.js
@@ -1,15 +1,40 @@
 function reversArray(array) {
-    var mid_index = array.length / 2;
-    console.log(mid_index);
+    const newArray = [];
+
     for (i = 0; i < array.length; i++) {
-        if (i > mid_index) {
-            array[i] = array[mid_index + 1]; // индекс 3,4,5
+
+        if (array.length % 2 == 0) {
+            /* устанавливаем начальное значение среднего индекса */
+            let mid_i = array.length / 2;
+            if (i > mid_i) {
+                for (j = 0; j < mid_i; j++) {
+                    let after = array[j];
+                    newArray[mid_i + j] = after;
+                }
+            }
+            if (i < mid_i) {
+                let before = array[mid_i + i];
+                newArray[i] = before;
+            }
         }
         else {
-            array[i] = array[mid_index + i]; // индекс 0,
+            let mid_odd_i = Math.round(array.length / 2);
+            if (i == mid_odd_i) {
+                newArray[mid_odd_i-1] = array[i-1];
+            }
+            if (i > mid_odd_i) {
+                for (j = 0; j < mid_odd_i - 1; j++) {
+                    let after_odd = array[j];
+                    newArray[mid_odd_i + j] = after_odd;
+                }
+            }
+            if (i < mid_odd_i) {
+                let before_odd = array[mid_odd_i + i];
+                newArray[i] = before_odd;
+            }
         }
     }
-    return array;
+    return newArray;
 }
 
-console.log(reversArray([1, 2, 3, 4, 5, 6]));
+console.log(reversArray([1, 2, 3, 4, 5, 8, 9, 10, 11]));
---
+++ 19_metodSelect.js
@@ -0,0 +1,19 @@
+function sortSelect(array) {
+    for (i = 0; i < array.length - 1; i++) {
+        /* устанавливаем начальное значение минимального индекса */
+                let min_i = i;
+            /* находим индекс минимального элемента */
+            for (j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min_i]) {
+                    min_i = j;
+                }
+            }
+            /* меняем значения местами */
+            let temp = array[i];
+            array[i] = array[min_i];
+            array[min_i] = temp;
+        }
+    return array;
+}
+
+console.log(sortSelect([5, 2, 1, 4, 3, 6]));
