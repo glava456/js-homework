0911cf2204ef480466d16b83c2a38b982acdcdf5
glava456 <glava456@gmail.com>
2021-07-27 15:17:58 +0300

    edit 22_changeWordToNum

--- 22_changeWordToNum.js
+++ 22_changeWordToNum.js
@@ -8,7 +8,6 @@ function changeNumToWord(str) {
     if (array[0] == 'ноль') {
         result.push(zero);
     }
-
     else {
         for (i = 0; i < array.length; i++) {
             var from0To19 = [
@@ -25,19 +24,73 @@ function changeNumToWord(str) {
                 'сто', 'двести', 'триста', 'четыреста', 'пятьсот',
                 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'
             ];
-
-
             if (array.length == 3) {
                 for (j = 0; j < hundreds.length; j++) {
-                    console.log (array[0]);
-                if (array[0] == hundreds[j]){
-                    result.push(j+1);
+                    if (array[0] == hundreds[j]) {
+                        result.push(j + 1);
+                    }
+                }
+
+                for (n = 0; n < tens.length; n++) {
+                    if (array[i + 1] == tens[n]) {
+                        result.push(n + 1);
+                    }
+
+                }
+                for (z = 0; z < from0To19.length; z++) {
+                    if (array[i + 2] == from0To19[z]) {
+                        result.push(z);
+                    }
                 }
             }
 
+            if (array.length == 2) {
+                for (j = 0; j < hundreds.length; j++) {
+                    if (array[i] == hundreds[j]) {
+                        result.push(j + 1);
+
+                    }
+                }
+
+                for (n = 0; n < tens.length; n++) {
+                    if (array[i] == tens[n]) {
+                        result.push(n + 1);
+                    }
+
+                }
+                for (z = 0; z < from0To19.length; z++) {
+                    if (array[i + 1] == from0To19[z]) {
+                        if (z < 9) {
+                            result.push(0);
+                        }
+                        result.push(z);
+                    }
+                }
+            }
+
+            if (array.length == 1) {
+                for (j = 0; j < hundreds.length; j++) {
+                    if (array[i] == hundreds[j]) {
+                        result.push(j + 1);
+                        result.push(0);
+                        result.push(0);
+                    }
+                }
+                for (n = 0; n < tens.length; n++) {
+                    if (array[i] == tens[n]) {
+                        result.push(n + 1);
+                        result.push(0);
+                    }
+
+                }
+                for (z = 0; z < from0To19.length; z++) {
+                    if (array[i] == from0To19[z]) {
+                        result.push(z);
+                    }
+                }
+            }
+            return Number(result.join(''));
         }
-        return result.join('');
     }
 }
-}
-    console.log(changeNumToWord('девятьсот двадцать три'));
+console.log(changeNumToWord('двенадцать'));
